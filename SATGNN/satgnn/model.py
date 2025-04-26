3ee67f3192b0bcb316ca8a700752ce4729477990
刘建飞 <3636786912@qq.com>
2025-04-26 11:18:35 +0800

    Initial commit

---
+++ SATGNN/satgnn/model.py
@@ -0,0 +1,129 @@
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+
+
+class ConvLayer(nn.Module):
+    """
+    Convolutional operation on graphs
+    """
+    def __init__(self, atom_fea_len, nbr_fea_len):
+        
+        super(ConvLayer, self).__init__()
+        self.atom_fea_len = atom_fea_len
+        self.nbr_fea_len = nbr_fea_len
+        self.fc_full = nn.Linear(2*self.atom_fea_len+self.nbr_fea_len,
+                                 2*self.atom_fea_len)
+        self.sigmoid = nn.Sigmoid()
+        self.softplus1 = nn.Softplus()
+        self.bn1 = nn.BatchNorm1d(2*self.atom_fea_len)
+        self.bn2 = nn.BatchNorm1d(self.atom_fea_len)
+        self.softplus2 = nn.Softplus()
+
+    def forward(self, atom_in_fea, nbr_fea, nbr_fea_idx):
+        
+        # TODO will there be problems with the index zero padding?
+        N, M = nbr_fea_idx.shape
+        # convolution
+        atom_nbr_fea = atom_in_fea[nbr_fea_idx, :]
+        total_nbr_fea = torch.cat(
+            [atom_in_fea.unsqueeze(1).expand(N, M, self.atom_fea_len),
+             atom_nbr_fea, nbr_fea], dim=2)
+        total_gated_fea = self.fc_full(total_nbr_fea)
+        total_gated_fea = self.bn1(total_gated_fea.view(
+            -1, self.atom_fea_len*2)).view(N, M, self.atom_fea_len*2)
+        nbr_filter, nbr_core = total_gated_fea.chunk(2, dim=2)
+        nbr_filter = self.sigmoid(nbr_filter)
+        nbr_core = self.softplus1(nbr_core)
+        nbr_sumed = torch.sum(nbr_filter * nbr_core, dim=1)
+        nbr_sumed = self.bn2(nbr_sumed)
+        out = self.softplus2(atom_in_fea + nbr_sumed)
+        return out
+
+
+class CrystalGraphConvNet(nn.Module):
+    """
+    Create a crystal graph convolutional neural network for predicting total
+    material properties.
+    """
+    def __init__(self, orig_atom_fea_len, nbr_fea_len,
+                 atom_fea_len=64, n_conv=3, h_fea_len=128, n_h=1,
+                 classification=False):
+        
+        super(CrystalGraphConvNet, self).__init__()
+        self.classification = classification
+        self.embedding = nn.Linear(orig_atom_fea_len, atom_fea_len)
+        self.convs = nn.ModuleList([ConvLayer(atom_fea_len=atom_fea_len,
+                                    nbr_fea_len=nbr_fea_len)
+                                    for _ in range(n_conv)])
+        self.conv_to_fc = nn.Linear(atom_fea_len*2, h_fea_len)
+        self.conv_to_fc_softplus = nn.Softplus()
+        if n_h > 1:
+            self.fcs = nn.ModuleList([nn.Linear(h_fea_len, h_fea_len)
+                                      for _ in range(n_h-1)])
+            self.softpluses = nn.ModuleList([nn.Softplus()
+                                             for _ in range(n_h-1)])
+        if self.classification:
+            self.fc_out = nn.Linear(h_fea_len, 2)
+        else:
+            self.fc_out = nn.Linear(h_fea_len, 1)
+        if self.classification:
+            self.logsoftmax = nn.LogSoftmax(dim=1)
+            self.dropout = nn.Dropout()
+            
+        input_dim = atom_fea_len
+        num_heads = 4
+        self.linear_q = nn.Linear(atom_fea_len, input_dim, bias=False)
+        self.linear_k = nn.Linear(atom_fea_len, input_dim, bias=False)
+        self.linear_v = nn.Linear(atom_fea_len, input_dim, bias=False)
+        self.multihead_attn = torch.nn.MultiheadAttention(embed_dim=input_dim, 
+                                                          num_heads=num_heads, 
+                                                          batch_first=True)
+        
+
+    def forward(self, atom_fea, nbr_fea, nbr_fea_idx, crystal_atom_idx):
+        
+        atom_fea = self.embedding(atom_fea)
+        for conv_func in self.convs:
+            atom_fea = conv_func(atom_fea, nbr_fea, nbr_fea_idx)
+        crys_fea = self.pooling(atom_fea, crystal_atom_idx)
+        
+        query = self.linear_q(crys_fea)
+        keys = self.linear_k(crys_fea)
+        values = self.linear_v(crys_fea)
+        output, _ = self.multihead_attn(query=query, key=keys, value=values)
+        # output = torch.mean(output, dim=1)
+        output = output.reshape(output.size(0), output.size(1)*output.size(2))
+        
+        crys_fea = self.conv_to_fc(self.conv_to_fc_softplus(output))
+        crys_fea = self.conv_to_fc_softplus(crys_fea)
+        if self.classification:
+            crys_fea = self.dropout(crys_fea)
+        if hasattr(self, 'fcs') and hasattr(self, 'softpluses'):
+            for fc, softplus in zip(self.fcs, self.softpluses):
+                crys_fea = softplus(fc(crys_fea))
+        out = self.fc_out(crys_fea)
+        if self.classification:
+            out = self.logsoftmax(out)
+        return out
+
+    def pooling(self, atom_fea, crystal_atom_idx):
+        
+        sum_atom = 0
+        for idx_map in crystal_atom_idx:
+            for abx_map in idx_map:
+                sum_atom += len(abx_map)
+        assert sum_atom == atom_fea.data.shape[0]
+        
+        abx = []
+        summed_fea = []
+        
+        for idx_map in crystal_atom_idx:
+            for abx_map in idx_map:
+                abx.append(torch.mean(atom_fea[abx_map], dim=0, keepdim=True))
+            summed_fea.append(torch.cat(abx, dim=0))
+            abx = []
+        # summed_fea = [torch.mean(atom_fea[idx_map], dim=0, keepdim=True)
+        #               for idx_map in crystal_atom_idx]
+        return torch.stack(summed_fea, dim=0)
